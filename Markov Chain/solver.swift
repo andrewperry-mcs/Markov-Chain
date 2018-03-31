14237f372c531ed6fd059a2323ad7d71b90291a7
Andrew Perry <andrewperry@AndrewscBookPro.socal.rr.com>
2018-03-31 12:49:29 -0700

    first commit

---
+++ Markov Chain/solver.swift
@@ -0,0 +1,9 @@
+//
+//  solver.swift
+//  Markov Chain
+//
+//  Created by Andrew Perry on 3/30/18.
+//  Copyright © 2018 Stanford University. All rights reserved.
+//
+
+import Foundation
---
+++ Markov Chain/vector.swift
@@ -0,0 +1,9 @@
+//
+//  vector.swift
+//  Markov Chain
+//
+//  Created by Andrew Perry on 3/29/18.
+//  Copyright © 2018 Stanford University. All rights reserved.
+//
+
+import Foundation
